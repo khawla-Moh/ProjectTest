f8828b8d10809f35a56b9d05367d625c5f7b2966
khawla-Moh <khawlaMoh00@gmail.com>
2023-11-18 23:34:27 +0300

    models

--- book/models.py
+++ book/models.py
@@ -1,4 +1,3 @@
-from datetime import timezone
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -9,20 +8,20 @@ from django.contrib.auth.models import User
 class Book(models.Model):
     author=models.ForeignKey(User,related_name='book_author',on_delete=models.CASCADE)    
    
-    name=models.CharField(max_length="100")
-    publish_date=models.DateTimeField(default=timezone.now)
+    name=models.CharField(max_length=100)
+    publish_date=models.DateTimeField()
     price=models.DecimalField(max_digits=8, decimal_places=2)
     
     def __str__(self):
         return self.name
 
 class Author(models.Model):
-    name=models.CharField(max_length="100")
-    birthdate_date=models.DateTimeField(default=timezone.now)
+    name=models.CharField(max_length=100)
+    birthdate_date=models.DateTimeField()
     biography=models.TextField(max_length=20000)
 
 class Review(models.Model):
     book=models.ForeignKey(Book,related_name='Review_book',on_delete=models.CASCADE)    
    
-    Reviewer_name=models.CharField(max_length="100")
+    Reviewer_name=models.CharField(max_length=100)
     content=models.TextField(max_length=20000)  
